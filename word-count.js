900523b562456c421ccf00466bb91b3f5e6e183d
Gus <ohnesorge.gus@gmail.com>
2022-01-12 11:57:18 -0600

    Basic word counting using spaces as separator

--- word-count.js
+++ word-count.js
@@ -3,6 +3,21 @@
 // convenience to get you started writing code faster.
 //
 
-export const countWords = () => {
-  throw new Error('Remove this statement and implement this function');
+export const countWords = (phrase) => {
+  phrase.toLowerCase()
+  let separate_words = phrase.split(" ")
+  let word_count = {}
+  for (let i = 0; i<separate_words.length; i++){
+    let word = separate_words[i]
+    let current_count = word_count[word]
+    // === avoiding type conversion
+    // could also check using slower 'in' property
+    if (current_count === undefined){
+      word_count[word] = 1
+    }
+    else{
+      word_count[word] = current_count+=1
+    }
+  }  
+  return word_count
 };
